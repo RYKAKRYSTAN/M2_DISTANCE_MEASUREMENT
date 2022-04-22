b472a7c40ff8078c73e415dee64b2f7555f99c2b
RYKAKRYSTAN <101564345+RYKAKRYSTAN@users.noreply.github.com>
2022-04-22 10:41:40 +0530

    Create distance.h

---
+++ 3-Implementation/inc/distance.h
@@ -0,0 +1,36 @@
+/**
+ * @file distance.h
+ * @author TEJAL (rtejal2001@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _distane_H_
+#define _distance_H_
+#ifndef _AVR_ATmega328p_
+   #define _AVR_ATmega32p_  
+#endif
+#define F_CPU 16000000UL					/* Define CPU Frequency e.g. here its 8MHz */
+#include <avr/io.h>						/* Include AVR std. library file */
+#include <util/delay.h>					/* Include Delay header file */
+
+#define LCD_Data_Dir DDRB				/* Define LCD data port direction */
+#define LCD_Command_Dir DDRC			/* Define LCD command port direction register */
+#define LCD_Data_Port PORTB				/* Define LCD data port */
+#define LCD_Command_Port PORTC			/* Define LCD data port */
+#define RS PC0							/* Define Register Select (data reg./command reg.) signal pin */
+#define RW PC1							/* Define Read/Write signal pin */
+#define EN PC2							/* Define Enable signal pin */
+
+void LCD_Command (char);				/* LCD command write function */
+void LCD_Char (char);					/* LCD data write function */
+void LCD_Init (void);					/* LCD Initialize function */
+void LCD_String (char*);				/* Send string to LCD function */
+void LCD_String_xy (char,char,char*);	/* Send row, position and string to LCD function */
+void LCD_Clear (void);					/* LCD clear function */
+
+
+#endif		
